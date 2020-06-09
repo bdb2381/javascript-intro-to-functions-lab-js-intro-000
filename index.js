32d1043d460faf56bc350ce5d6982b31e58277dc
Bradley Beggs <brad.beggs@gmail.com>
2020-06-09 02:33:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-function shout(string){
+function shout(string){         //function returns strings in UPPERCASE
 
   return string.toUpperCase()
 }
 
-function whisper(string){
+function whisper(string){       //function returns strings in lowercase
   return string.toLowerCase()
+}
 
+
+function logShout(string){
+  console.log(string.toUpperCase())
 }
