5ac7f8e8cbf8090cf78a2f2cfbad89547279716b
Bradley Beggs <brad.beggs@gmail.com>
2020-06-09 02:45:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function logWhisper(string){    //logs the string in lowercase
   console.log(string.toLowerCase())
 }
 
-function sayHiToGrandma(string){
+function sayHiToGrandma(string){  //make sure grandma can hear us by checking cAsE
   if (string === string.toLowerCase()) {
     return "I can't hear you!"
   }
@@ -25,4 +25,6 @@ function sayHiToGrandma(string){
     return "YES INDEED!"
   }
 
+  else if (string === "I love you, Grandma.")
+    return "I love you, too."
 }
